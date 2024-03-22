69cc4ae017f4695c6aab4ad658a6acfa2788433f
Om Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2024-03-22 22:55:33 +0530

    Added BotConfig
    Env vars, token n username (bot)

---
+++ src/main/java/org/cyberpsych/BotConfig.java
@@ -0,0 +1,7 @@
+package org.cyberpsych;
+public class BotConfig {
+    public static final String DB_NAME = "tod";
+    public static final String COLLECTION_NAME = "tasks";
+    public static final String TOD_TOKEN = "YOUR_BOT_TOKEN"; //API-Token
+    public static final String TOD_USER = "YOUR_BOT_USERNAME"; //Bot-Username
+}
